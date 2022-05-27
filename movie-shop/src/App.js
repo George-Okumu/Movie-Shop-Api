a4ecb9a71cef2a4d1fe4ed896deaff35c43c414e
George-Okumu <gokumu12@gmail.com>
2022-05-27 08:54:58 +0300

    installed React routing

--- movie-shop/src/App.js
+++ movie-shop/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Movies from './Movies';
 
 function App() {
   return (
     <>
-      <Movies/>
+      <Routes>
+        <Route exact="true" path='/home' element={<Movies/>} />
+      </Routes>
     </>
   );
 }
